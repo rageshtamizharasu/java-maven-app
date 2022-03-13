1e476cee9e1b6c52943e856e6e6297b23cf369ee
ragesh <ragesh@gmail.com>
2022-03-13 20:50:44 +0530

    updated the script.groovy

--- script.groovy
+++ script.groovy
@@ -4,13 +4,14 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-        //* if docker fail make sure to change the permission chmod 777 /var/run/docker.sock inside jenkins container
-    }
+    
+                echo " Building the Docker image & Pushing into DockerHub"
+                 withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                 sh 'docker build -t ragesh2u/my-repo:jvm-2.0  .'
+                 sh "echo $PASS | docker login -u $USER --password-stdin"
+                 sh 'docker push ragesh2u/my-repo:jvm-2.0'
+                 //* if docker fail /change the permission chmod 777 /var/run/docker.sock inside jenkins container using root -u 0
+                }
 } 
 
 def deployApp() {
